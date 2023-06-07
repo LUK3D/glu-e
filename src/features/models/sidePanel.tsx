701ace9fe5b07b244bf84ea9dc81f240a8385553
Filipe Lukebana <filipelukebana@gmail.com>
2023-06-07 13:26:15 -0300

    Added placeholder

--- src/features/models/sidePanel.tsx
+++ src/features/models/sidePanel.tsx
@@ -8,8 +8,9 @@ export  function SidePanel(args:ISidePanel) {
     console.log(args.appStore.workspace);
 
   return (
-    <div className="flex flex-col h-full w-full">
-      
+    <div className="flex flex-col h-full w-full ">
+
+        
 
     </div>
   )
--- src/features/models/workspace.tsx
+++ src/features/models/workspace.tsx
@@ -1,8 +1,8 @@
 
 export  function Workspace() {
   return (
-    <div className="w-full h-full overflow-y-auto flex flex-col p-5">
-     
+    <div className="w-full h-full overflow-y-auto flex flex-col p-5 text-4xl">
+        <h1>Models will be here</h1>
     </div>
   )
 }
